4749a40d841c8dd814b06e3b542e7925abbeee9a
Randil Hasanga <randil_2020690@fot.ruh.ac.lk>
2024-10-31 19:12:55 +0530

    from mssql to mysql

--- backend/controllers/testController.js
+++ backend/controllers/testController.js
@@ -1,9 +1,18 @@
-const mssql = require('mssql');
+const db = require('../db');
 
 const getDemoData = async (req, res) => {
   try {
-    const pool = await mssql.connect();
-    const result = await pool.request().query('SELECT TOP 5 * FROM DemoTable');
+    const sql = "SELECT * FROM Patients";
+
+    db.query(sql, (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ message: "Something unexpected has occurred" });
+      }
+      console.log('Data retrieved successfully');
+      return res.status(200).json(result);
+    });
+
   } catch (err) {
     console.log(err);
     res.status(500).send('Database query failed');
