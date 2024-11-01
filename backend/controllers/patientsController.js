3822c295701f2a5ffbb5941a757675af3326f905
Randil Hasanga <randil_2020690@fot.ruh.ac.lk>
2024-11-01 10:44:31 +0530

    created patients controller
    getPatients
    getPatientsByID

---
+++ backend/controllers/patientsController.js
@@ -0,0 +1,44 @@
+const db = require('../db');
+
+const getPatients = async (req, res) => {
+  try {
+    const sql = "SELECT * FROM Patients";
+
+    db.query(sql, (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ message: "Something unexpected has occurred" });
+      }
+      console.log('Data retrieved successfully');
+      return res.status(200).json(result);
+    });
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Database query failed');
+  }
+};
+
+const getPatientsByID = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const sql = "SELECT * FROM Patients WHERE PatientID = ?";
+
+        db.query(sql,[id], (err, result) => {
+            if (err) {
+              console.log(err);
+              return res.status(500).json({ message: "Something unexpected has occurred" });
+            }
+            console.log('Data retrieved successfully');
+            return res.status(200).json(result);
+          });
+    } catch (error) {
+        console.log(err);
+        res.status(500).send('Database query failed');
+    }
+}
+
+module.exports = {
+   getPatients,
+   getPatientsByID
+};
